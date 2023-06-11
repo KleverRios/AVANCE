c8db3c20d09dbb07e4fa26b909b3b777c1934530
KleverRios <108413636+KleverRios@users.noreply.github.com>
2023-06-11 11:48:21 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+$servidor = "localhost";
+$usuario = "root";
+$clave = "123456";
+$bd = "proytw";
+$conexion = mysqli_connect($servidor, $usuario, $clave, $bd);
+/*if($conexion){
+    echo 'conexion exitosa';
+}else {
+    echo 'conexion fallida';
+}*/
+?>
